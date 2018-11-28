f14c555172e6364d8e702f283dc1fd5027ddfa1b
enridaga <enricodaga@gmail.com>
2018-11-28 11:24:24 +0000

    Updated virtuoso experiments

--- experiments/virtuoso.sh
+++ experiments/virtuoso.sh
@@ -11,7 +11,7 @@ do
     if [[ -f $file ]]; then
 		# PUT into virtuoso
 		data=$(basename "${file%.*}")
-		echo put-in-virtuoso.sh $file "data:$data"
+		./put-in-virtuoso.sh $file "data:$data"
     fi
 done
 
@@ -22,11 +22,12 @@ do
 	# there should be only files anyway
     if [[ -f $file ]]; then
 		data=$(basename "${file%.*}")
-		export QUERY_GRAPH="file:$file"
+		export QUERY_GRAPH="data:$data"
 		line="${data//-/$IFS}"
 		arr=($line)
 		eid=virtuoso-$data
 		suite=${arr[1]}.txt
-		echo "./run-experiment.sh $virtuoso_pid $eid suite/$suite 1 3 10"
+		# script PID experimentID suite times interval timeout
+		./run-experiment.sh $virtuoso_pid $eid suite/$suite 1 3 10
     fi
 done
