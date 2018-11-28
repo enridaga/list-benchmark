1af2c2e2dc22c59cb1ea60c31ac029583fcb02a1
enridaga <enricodaga@gmail.com>
2018-11-28 15:30:39 +0000

    Fixed query and execution order

--- experiments/virtuoso-run.sh
+++ experiments/virtuoso-run.sh
@@ -6,8 +6,9 @@ virtuoso_pid=$1
 
 # Run experiments
 export QUERY_ENDPOINT=http://localhost:8890/sparql
-for file in ../data/*
+for file in $(ls -Sr ../data/*)
 do
+    echo "Performing tests on data $file"
 	# there should be only files anyway
     if [[ -f $file ]]; then
 		data=$(basename "${file%.*}")
@@ -17,6 +18,6 @@ do
 		eid=virtuoso-$data
 		suite=${arr[1]}.txt
 		# script PID experimentID suite times interval timeout
-		./run-experiment.sh $virtuoso_pid $eid suite/$suite 10 5 60
+		./run-experiment.sh $virtuoso_pid $eid suite/$suite 10 5 300
     fi
 done
