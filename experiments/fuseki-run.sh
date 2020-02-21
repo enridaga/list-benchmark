acd84db29c8ea74873704fb11bf3f64107c5e6bd
enridaga <enricodaga@gmail.com>
2020-02-21 12:39:38 +0000

    changes for sparql updates

--- experiments/fuseki-run.sh
+++ experiments/fuseki-run.sh
@@ -6,13 +6,17 @@ fuseki_pid=$1
 prefix=${2:-fuseki}
 # Run experiments
 export QUERY_ENDPOINT=http://localhost:3030/ds/sparql
-for file in $(ls -Sr ../data/*|grep "$grep")
+export UPDATE_ENDPOINT=http://localhost:3030/ds/update
+for file in $(ls -Sr ../data/*.ttl|grep "$grep")
 do
     echo "Performing tests on data $file"
     # there should be only files anyway
     if [[ -f $file ]]; then
 		data=$(basename "${file%.*}")
 		export QUERY_GRAPH="data:$data"
+		echo "Graph: "$QUERY_GRAPH
+		export QUERY_TRACK=$(./get_query_track.sh $data)
+		echo "Track: "$QUERY_TRACK
 		line="${data//-/$IFS}"
 		arr=($line)
 		eid=$prefix-$data
