39d77cff30b8853fbe48d25ad8088c7438fd1a4e
enridaga <enricodaga@gmail.com>
2020-02-26 09:49:11 +0000

    Missing in last commit

--- experiments/query-endpoint.sh
+++ experiments/query-endpoint.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-function encodeURIComponent {
-	perl -pe 's/([^a-zA-Z0-9_.!~*()'\''-])/sprintf("%%%02X", ord($1))/ge'
-}
-function setGraph {
-	ghost="http://purl.org/midi-ld/piece/2eb43ce7edf27b505bcc0dfb6c283784"
-	graph=$QUERY_GRAPH
-	sed "s,$ghost,$graph,g"
-}
-function setTrack {
-	ghost="http://purl.org/midi-ld/piece/2473e18eec6cc55b82c5dddab3bea353/track00"
-	track=$QUERY_TRACK
-	sed "s,$ghost,$track,g"	
-}
+source ./prepare_query_functions.sh
 
 queryfile=$1
 endpoint=$QUERY_ENDPOINT
-query=`cat $queryfile|setGraph|setTrack`
+query=`cat $queryfile|prepareQuery`
 >&2 echo "-----------------------------------"
 >&2 echo "Querying $endpoint, file $queryfile"
 >&2 echo "Query: $query"
