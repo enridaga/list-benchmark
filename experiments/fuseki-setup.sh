ddf2e2ef61996221a5a777999c450eeb276212e8
enridaga <enricodaga@gmail.com>
2020-03-19 17:52:45 +0000

    changes to fuseki-setup

--- experiments/fuseki-setup.sh
+++ experiments/fuseki-setup.sh
@@ -8,7 +8,7 @@ do
     if [[ -f $file ]]; then
 		# PUT into fuseki
 		data=$(basename "${file%.*}")
-		curl -X PUT http://localhost:3030/ds/data -G -T $(realpath $file) --data-urlencode graph="data:$data" -H "Content-Type:text/turtle"
+		curl -X PUT http://localhost:3030/ds/data -G -T $(realpath $file) --data-urlencode graph="data:$data" -H "Content-Type: text/turtle"
 		sleep 1
     fi
 done
