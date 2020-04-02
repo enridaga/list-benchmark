6a231da465ba60112c144980c3da37cf20c69eac
enridaga <enricodaga@gmail.com>
2020-04-02 10:58:55 +0100

    Supporting regex on grep

--- experiments/blazegraph-run.sh
+++ experiments/blazegraph-run.sh
@@ -7,21 +7,21 @@ source prepare_query_functions.sh
 server_pid=$1
 
 # Run experiments
-for file in $(ls -Sr ../data/*.ttl|grep "$grep")
+for file in $(ls -Sr ../data/*.ttl|grep -E "$grep")
 do
     echo "Performing tests on data $file"
 	# there should be only files anyway
     if [[ -f $file ]]; then
 		data=$(basename "${file%.*}")
 		prepareEnvironment $data blazegraph
- 		echo " > params: "$QUERY_GRAPH" "$QUERY_TRACK" "$QUERY_RANDOM
+ 		#echo " > params: "$QUERY_GRAPH" "$QUERY_TRACK" "$QUERY_RANDOM
 		line="${data//-/$IFS}"
 		arr=($line)
 		eid=blazegraph-$data
 		suite=${arr[1]}.txt
 		# script PID experimentID suite times interval timeout
-		#echo "./run-experiment.sh $server_pid $eid suite/$suite 10 5 300"
-		./run-experiment.sh $server_pid $eid suite/$suite 10 5 300
+		echo "./run-experiment.sh $server_pid $eid suite/$suite 10 5 300"
+		#./run-experiment.sh $server_pid $eid suite/$suite 10 5 300
 		#./run-experiment.sh $server_pid $eid suite/$suite 1 1 10
     fi
 done
